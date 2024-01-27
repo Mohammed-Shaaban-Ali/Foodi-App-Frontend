136b86cd4730adf9843a64d46103715e57ccf3ba
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2024-01-27 17:09:38 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,56 @@
 import React from "react";
 import logo from "/logo.png";
+import { BiPhoneCall } from "react-icons/bi";
 
 const Navbar = () => {
+  const navItem = (
+    <>
+      <li>
+        <a href="/">Home</a>
+      </li>
+
+      <li>
+        <details>
+          <summary>Menu</summary>
+          <ul className="p-2">
+            <li>
+              <a>All</a>
+            </li>
+            <li>
+              <a>Salad</a>
+            </li>
+            <li>
+              <a>Pizza</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+
+      <li className="static">
+        <details>
+          <summary>Services</summary>
+          <ul className="p-2">
+            <li>
+              <a>Online Order</a>
+            </li>
+            <li>
+              <a>Table Booking</a>
+            </li>
+            <li>
+              <a>Order Tracking</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+
+      <li>
+        <a href="/">Offers</a>
+      </li>
+    </>
+  );
   return (
-    <nav>
-      <div className="navbar bg-base-100">
+    <header className="max-w-screen-2xl container mx-auto">
+      <div className="navbar xl:px-24">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -27,57 +73,66 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navItem}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">
+          <a href="/">
             <img src={logo} alt="logo" />
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navItem}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <button className="btn btn-ghost btn-circle hidden lg:flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          {/* cart item */}
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle mr-1 lg:mr-3 items-center justify-center"
+          >
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item bg-green text-white">
+                8
+              </span>
+            </div>
+          </div>
+          <a className="btn bg-green rounded-full text-white px-6 flex items-center gap-3 text-lg">
+            <BiPhoneCall className="" />
+            Contact
+          </a>
         </div>
       </div>
-    </nav>
+    </header>
   );
 };
 
